f89e81a5a0d8076b8eec435dcfbe052c13f6ef95
Suchaowut <87254783+Suchaowut@users.noreply.github.com>
2022-09-24 23:25:39 +0700

    Y

--- index.js
+++ index.js
@@ -56,4 +56,4 @@ jointocreateprivate(client);
 const jointocreatetalk = require("./jointocreate/CreateTalk.js");
 jointocreatetalk(client);
 
-client.login(process.env.token);
+client.login('ODc4OTg3OTI2Njg2NTU2MjAy.GtKA95.bvgrKVPll84yu-D-EEETfC0BNDSpbXpsufCRQI');
